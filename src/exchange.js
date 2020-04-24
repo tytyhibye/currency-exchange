6cdac779f26a83344188012459f4bbfc642f188a
Tyler Bates <Fondlytyler@gmail.com>
2020-04-24 09:21:54 -0700

    add API call function with return framework.

---
+++ src/exchange.js
@@ -0,0 +1,22 @@
+export class Xchange {
+  async getRate() {
+   
+    let jsonifiedResponse;
+    try {
+      let response = await fetch(` https://prime.exchangerate-api.com/v5/${process.env.API_KEY}`);
+      // console.log(await);
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json(); 
+      } else {
+        jsonifiedResponse = false;
+      }
+    } catch (error) {
+      // console.log(catch);
+      return false;
+    }  
+    return jsonifiedResponse;  
+  }
+
+  
+  
+}
--- src/main.js
+++ src/main.js
@@ -2,3 +2,23 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+import { Xchange } from './exchange.js';
+
+$(document).ready(function () {
+  $('#button').click(function () {
+    (async () => {
+      let xchange = new Xchange();
+      const response = await xchange.getRate();
+      console.log('response: ', response);
+      getElements(response);
+    })();
+    function getElements(response) {
+      if (response) {
+        let rate = response.filler
+        $('#result').text(rate)
+      } else {
+        $('#result').text('Uh oh, something went wrong. Make sure you have the proper CORS plugin/permission and try again.');
+      }
+    }
+    });
+  })
