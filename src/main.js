07c28f6a433b4e74bd3264348ddf872c75239d23
Tyler Bates <Fondlytyler@gmail.com>
2020-04-24 16:10:51 -0700

    add site path byt revealing results upon submit and hiding form information.

--- src/main.js
+++ src/main.js
@@ -17,6 +17,8 @@ $(document).ready(function () {
     function getElements(response) {
       if (response) {
         $('#result').text(parseInt(response * inputAmount));
+        $('#input-form').hide()
+        $('#results').fadeIn(2000);
       } else {
         $('#result').text("Uh oh, this doesn't seem to be a recognized currency. Please select another from the list.");
       }
